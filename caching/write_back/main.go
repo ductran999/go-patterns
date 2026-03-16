b2f3301b6af3aa19ca70a2c269e28378cb185d13
Daniel <74949792+ductran999@users.noreply.github.com>
2026-03-16 20:47:56 +0700

    chore: lint project

--- caching/write_back/main.go
+++ caching/write_back/main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"crypto/rand"
+	"errors"
 	"log"
 	"maps"
+	"math/big"
 	"sync"
 	"time"
 )
 
 type Database struct{}
 
+func randomFail(prob int64) bool {
+	n, _ := rand.Int(rand.Reader, big.NewInt(100))
+	return n.Int64() < prob
+}
+
 func (db *Database) UpdateLastSeenBatch(batch map[string]time.Time) error {
-	fmt.Printf("[DB] Updating last_seen for %d agents to Database...\n", len(batch))
+	log.Printf("[DB] Updating last_seen for %d agents to Database...\n", len(batch))
+
 	for agentID, lastSeen := range batch {
-		fmt.Printf("      -> DB Update: Agent %s | Last Seen: %s\n", agentID, lastSeen.Format("15:04:05"))
+		log.Printf("      -> DB Update: Agent %s | Last Seen: %s\n",
+			agentID, lastSeen.Format("15:04:05"))
+	}
+
+	if randomFail(30) { // 30% failure
+		return errors.New("simulated database write failure")
 	}
 
 	return nil
@@ -88,7 +101,7 @@ func main() {
 		for range 12 {
 			manager.ReceiveHeartbeat("agent_001")
 			manager.ReceiveHeartbeat("agent_002")
-			fmt.Println("[App] Got heartbeat from agent_001 & agent_002")
+			log.Println("[App] Got heartbeat from agent_001 & agent_002")
 			time.Sleep(1 * time.Second)
 		}
 	}()
